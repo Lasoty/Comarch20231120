76c1f744c3d67215d04589ebf63f862f676b5683
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2023-11-22 11:25:11 +0100

    * Implementacja Blazorise * Model encji

---
+++ Bibliotekarz/Bibliotekarz/Server/Controllers/BookController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bibliotekarz.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        public BookController()
+        {
+
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetBooks()
+        {
+            //return StatusCode(StatusCodes.Status201Created);
+            //return NoContent();
+            //return BadRequest();
+            //return NotFound();
+
+
+            return Ok();
+        }
+            
+
+    }
+}
---
+++ Bibliotekarz/Bibliotekarz/Shared/Model/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Bibliotekarz.Shared.Model;
+
+public class Customer
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; }
+    
+    public string LasttName { get; set; }
+}
